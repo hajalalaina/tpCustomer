44fbde8876734a7a4e10e6b7895b0c23df09bab4
hajalalaina <rahajalalaina@yahoo.fr>
2022-03-25 10:45:27 +0300

    implementation du backing bean de la page customerlist.xhtml

---
+++ src/main/java/mg/mbds/tpcustomer/config/ConfigJSF.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author Hajalalaina
+ */
+
+package mg.mbds.tpcustomer.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
+
